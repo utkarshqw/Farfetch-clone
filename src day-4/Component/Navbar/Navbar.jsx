849b4dc750498e02110dcd4a5dedbfa86dac6872
utkarshqw <utkarshup75@gmail.com>
2022-07-22 15:46:34 +0530

    day-4

---
+++ src day-4/Component/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import  "./Navbar.css"
+
+function Navbar(){
+  return (
+    <>
+   
+<div className="topbar">
+  <div>sale</div>
+  <div>|</div>
+  <div>10,000 items at 60% off</div>
+  <div>|</div>
+  <div>shop now</div>
+</div>
+
+
+
+    </>
+  )
+}
+
+export default Navbar
---
+++ src day-4/Component/NewIn/Newindata.jsx
@@ -0,0 +1,101 @@
+
+
+export var newindata = [
+    {
+        id:1,
+        img:"https://cdn-images.farfetch-contents.com/17/98/28/10/17982810_40578835_480.jpg",
+        one:"New Season",
+        two: "Jean Paul Gaultier",
+        three:"button-fastening denim top",
+        four:"$1,477"
+    },
+    {
+        id:2,
+        img:"https://cdn-images.farfetch-contents.com/17/98/28/35/17982835_40581246_480.jpg",
+        one:"New Season",
+        two: "Jean Paul Gaultier",
+        three:"straight-leg denim jeans",
+        four:"$848"
+    },
+    {
+        id:3,
+        img:"https://tpc.googlesyndication.com/simgad/5969350101145511490?",
+        one:"Featured",
+        two: "Balmain",
+        three:"Shop Now",
+        four:"$200"
+    },
+    {
+        id:4,
+        img:"https://cdn-images.farfetch-contents.com/18/03/81/22/18038122_40589906_480.jpg",
+        one:"New Season",
+        two: "Alexander McQueen",
+        three:"logo-print crossbody bag",
+        four:"$890"
+    },
+    {
+        id:5,
+        img:"https://cdn-images.farfetch-contents.com/18/68/06/85/18680685_40553673_480.jpg",
+        one:"New Season",
+        two: "Balmain",
+        three:"double-breasted cropped sleeve blazer",
+        four:"$2,246"
+    },
+    {
+        id:6,
+        img:"https://cdn-images.farfetch-contents.com/18/75/17/84/18751784_40630977_480.jpg",
+        one:"New Season",
+        two: "Versace",
+        three:"Medusa 115mm sandals",
+        four:"$1,758"
+    },
+    {
+        id:7,
+        img:"https://cdn-images.farfetch-contents.com/18/60/08/02/18600802_40321775_480.jpg",
+        one:"New Season",
+        two: "Versace",
+        three:"I Ventagli midi shirt dress",
+        four:"$3,243"
+    },
+    {
+        id:8,
+        img:"https://cdn-images.farfetch-contents.com/17/96/74/97/17967497_39521094_480.jpg",
+        one:"New Season",
+        two: "Versace",
+        three:"Mdeusa detail skirt",
+        four:"$1,618"
+    },
+    {
+        id:9,
+        img:"https://cdn-images.farfetch-contents.com/18/70/22/70/18702270_40585635_480.jpg",
+        one:"New Season",
+        two: "Moschino",
+        three:"quilted logo-patch shoulder bag",
+        four:"$585"
+    },
+    {
+        id:10,
+        img:"https://cdn-images.farfetch-contents.com/18/36/37/60/18363760_40556339_480.jpg",
+        one:"New Season",
+        two: "Valentino",
+        three:"zebra-print shorts",
+        four:"$1,051"
+    },
+    {
+        id:11,
+        img:"https://cdn-images.farfetch-contents.com/18/36/52/34/18365234_39976106_480.jpg",
+        one:"New Season",
+        two: "Valentino",
+        three:"VGOLD denim shorts",
+        four:"$769"
+    },
+    {
+        id:12,
+        img:"https://cdn-images.farfetch-contents.com/18/36/42/35/18364235_40046944_480.jpg",
+        one:"New Season",
+        two: "Valentino",
+        three:"striped knitted polo dress",
+        four:"$2,123"
+    }
+
+]
