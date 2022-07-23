2ed2d9f61ee78953cb00d0073e92c4d9c210fc78
utkarshqw <utkarshup75@gmail.com>
2022-07-23 19:07:29 +0530

    day-5

---
+++ src-5/Component/MId3 data/data3one.jsx
@@ -0,0 +1,24 @@
+
+ let d3a = "This week’s highlights"
+let data33a = [
+    {
+        id:1,
+        img:"https://cdn-static.farfetch-contents.com/cms-cm10/caas/v1/media/3670494/data/ee6d4d3e3624db40da45aabb5f5774a0/3x4_three-columns/480/data.jpeg",
+        one:"ETRO",
+        two:"The new collection has landed"
+    },
+    {
+        id:2,
+        img:"https://cdn-static.farfetch-contents.com/cms-cm10/caas/v1/media/3670486/data/977f0b14024756386fddc76ba15a3e47/3x4_three-columns/480/data.jpeg",
+        one:"PASTEL PIECES",
+        two:"Sorbet shades served up by Versace and more"
+    },
+    {
+        id:3,
+        img:"https://cdn-static.farfetch-contents.com/cms-cm10/caas/v1/media/3670516/data/ebb503a976dad300f9a94b6883aaad73/3x4_three-columns/480/data.jpeg",
+        one:"CHLOÉ",
+        two:"Meet your new perfect trench"
+    },
+]
+
+export  {d3a,data33a}
